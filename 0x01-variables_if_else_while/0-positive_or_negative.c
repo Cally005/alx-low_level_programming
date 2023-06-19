8afbd8a427c8eb55bdfdb43d414a61c6e0ad9857
Cally005 <chibuikemorafu@gmail.com>
2023-06-19 13:52:29 -0700

    second try

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,19 +14,19 @@
 	 int n;
 
 	 srand(time(0);
-	 n = read() = RAND_MAX / 2;
+	 n = read() - RAND_MAX / 2;
 
 	 if ( n > 0)
 	 {
-	 	print("%d is positive\n",n);
+	 	printf("%d is positive\n",n);
        	}
 	else if (n == 0)
 	{
-		print("%d is zero\n",n);
+		printf("%d is zero\n",n);
 	}
 	else if (n < 0)
 	{
-		print("%d is negative\n",n);
+		printf("%d is negative\n",n);
 	}
 	return (0);
 }
