6c17ce86afaf6f966f3cf27c7b19954106c79308
Cally005 <chibuikemorafu@gmail.com>
2023-06-20 06:34:05 -0700

    tabstop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,9 +6,9 @@
  * Return: Always 0
  */
 
-int main(void)
+ int main(void)
 
-{
+ {
 
 	 _putchar('_');
 	 _putchar('p');
@@ -20,4 +20,4 @@ int main(void)
 	 _putchar('r');
 	 _putchar('\n');
 	 return (0);
-}
+ } 
