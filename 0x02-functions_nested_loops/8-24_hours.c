538ad155cd74f0bad84a660a8d0dba94e3b2b0e1
Cally005 <chibuikemorafu@gmail.com>
2023-06-20 12:45:53 -0700

    8.24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,14 +5,15 @@
  * Return: ...
  */
 
- void jack_bauer(void)
- {
+void jack_bauer(void)
 
- 	int  a, b, c, d;
-	
+{
+
+	int  a, b, c, d;
+
 	for (a = 0; a <=2; a++)
 	{
-        for (b = 0; b <= 9; b++)
+	for (b = 0; b <= 9; b++)
 	{
 	if ((a <= 1 && b <= 9) || (a <= 2 && b <=9))
 	{
@@ -21,7 +22,7 @@
 	for (d = 0; d <= 9; d++)
 	{
 	_putchar(a + '0');
-	_putchar(b + '0');	
+	_putchar(b + '0');
 	_putchar(58);
 	_putchar(c + '0');
 	_putchar(d + '0');
