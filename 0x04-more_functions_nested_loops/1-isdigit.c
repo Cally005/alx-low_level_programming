d6fe79526fc2aab2defb058e04410dac17fa8ea8
Cally005 <chibuikemorafu@gmail.com>
2023-06-22 07:41:57 -0700

    fixed betty

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,7 +3,7 @@
 /**
 * _isdigit - Checks if th charater is digit
 * @x: The number to be chechked
-* Retutns: 1 for a  character that will be a digit 0 for any else
+* Return: 1 for a  character that will be a digit 0 for any else
 */
 
 int _isdigit(int x)
